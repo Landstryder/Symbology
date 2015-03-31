236605da567c21560efb3efd0c4227ca45910a77
saxon564 <saxon564@gmail.com>
2015-03-30 22:48:56 -0400

    Added example

--- src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
+++ src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
@@ -1,5 +1,13 @@
 package com.teamrune.symbology.registers;
 
+import com.teamrune.symbology.Symbology;
+
+import net.minecraftforge.oredict.OreDictionary;
+
 public class RegisterOreDict {
+	
+	public static void AddOres() {
+		OreDictionary.registerOre("blockSlate", Symbology.slate_block);
+	}
 
 }
