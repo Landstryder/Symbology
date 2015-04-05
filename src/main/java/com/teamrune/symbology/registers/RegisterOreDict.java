43ef31fc6e2fff4e3820781a15eadda099c9a506
Landstryder <brian@bicyclefrontier.com>
2015-04-05 02:27:07 -0400

    maybe fix for ash plank

--- src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
+++ src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
@@ -2,16 +2,17 @@ package com.teamrune.symbology.registers;
 
 import com.teamrune.symbology.Symbology;
 
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class RegisterOreDict {
 	
 	public static void AddOres() {
-		OreDictionary.registerOre("blockSlate", Symbology.slate_block);
-        OreDictionary.registerOre("plankWood", Symbology.ash_plank);
-        OreDictionary.registerOre("logWood", Symbology.ash_log);
-        OreDictionary.registerOre("oreSilver", Symbology.silver_ore);
-        OreDictionary.registerOre("oreRuby", Symbology.ruby_ore);
+		OreDictionary.registerOre("blockSlate", new ItemStack(Symbology.slate_block));
+        OreDictionary.registerOre("plankWood", new ItemStack(Symbology.ash_plank));
+        OreDictionary.registerOre("logWood", new ItemStack(Symbology.ash_log));
+        OreDictionary.registerOre("oreSilver", new ItemStack(Symbology.silver_ore));
+        OreDictionary.registerOre("oreRuby", new ItemStack(Symbology.ruby_ore));
 	}
 
 }
