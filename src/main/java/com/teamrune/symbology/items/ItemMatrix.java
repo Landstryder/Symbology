b40e9337dd0846731f1167dd7a1fdd9e73fe0bf0
Landstryder <brian@bicyclefrontier.com>
2015-04-05 21:34:51 -0400

    Scale objects, Making the matrix open crafting grid

--- src/main/java/com/teamrune/symbology/items/ItemMatrix.java
+++ src/main/java/com/teamrune/symbology/items/ItemMatrix.java
@@ -1,7 +1,12 @@
 package com.teamrune.symbology.items;
 
 import com.teamrune.symbology.Symbology;
+import net.minecraft.block.BlockWorkbench;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemMatrix extends Item
 {
@@ -13,4 +18,11 @@ public class ItemMatrix extends Item
             setCreativeTab(Symbology.symbologyTab);
             setUnlocalizedName("rune_matrix");
         }
+
+    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
+    {
+
+        playerIn.displayGui(new BlockWorkbench.InterfaceCraftingTable(worldIn, BlockPos.ORIGIN));
+        return itemStackIn;
+    }
 }
--- src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
+++ src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
@@ -13,6 +13,16 @@ public class RegisterOreDict {
         OreDictionary.registerOre("logWood", Symbology.ash_log);
         OreDictionary.registerOre("oreSilver", Symbology.silver_ore);
         OreDictionary.registerOre("oreRuby", Symbology.ruby_ore);
+        OreDictionary.registerOre("oreTopaz", Symbology.topaz_ore);
+        OreDictionary.registerOre("oreAventurine", Symbology.aventurine_ore);
+        OreDictionary.registerOre("ingotSilver", Symbology.silver_ingot);
+        OreDictionary.registerOre("dustSilver", Symbology.silver_dust);
+        OreDictionary.registerOre("dustRuby", Symbology.ruby_dust);
+        OreDictionary.registerOre("gemRuby", Symbology.ruby_gem);
+        OreDictionary.registerOre("dustTopaz", Symbology.topaz_dust);
+        OreDictionary.registerOre("gemTopaz", Symbology.topaz_gem);
+        OreDictionary.registerOre("dustAventurine", Symbology.aventurine_dust);
+        OreDictionary.registerOre("gemAventurine", Symbology.aventurine_gem);
+        OreDictionary.registerOre("dustCharcoal", Symbology.charcoal_dust);
 	}
-
 }
