fa4d21e0848c382f1babfe37d03e6e2f9ac12d25
Landstryder <brian@bicyclefrontier.com>
2015-04-05 20:22:44 -0400

    OreDict change

--- src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
+++ src/main/java/com/teamrune/symbology/registers/RegisterOreDict.java
@@ -8,11 +8,11 @@ import net.minecraftforge.oredict.OreDictionary;
 public class RegisterOreDict {
 	
 	public static void AddOres() {
-		OreDictionary.registerOre("blockSlate", new ItemStack(Symbology.slate_block, 1));
-        OreDictionary.registerOre("plankWood", new ItemStack(Symbology.ash_plank, 1));
-        OreDictionary.registerOre("logWood", new ItemStack(Symbology.ash_log, 1));
-        OreDictionary.registerOre("oreSilver", new ItemStack(Symbology.silver_ore, 1));
-        OreDictionary.registerOre("oreRuby", new ItemStack(Symbology.ruby_ore, 1));
+		OreDictionary.registerOre("blockSlate", Symbology.slate_block);
+        OreDictionary.registerOre("plankWood", Symbology.ash_plank);
+        OreDictionary.registerOre("logWood", Symbology.ash_log);
+        OreDictionary.registerOre("oreSilver", Symbology.silver_ore);
+        OreDictionary.registerOre("oreRuby", Symbology.ruby_ore);
 	}
 
 }
