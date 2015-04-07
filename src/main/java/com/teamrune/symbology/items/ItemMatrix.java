8b55ef0564498b331c45384980ab0a2a29eeb060
saxon564 <saxon564@gmail.com>
2015-04-07 00:30:43 -0400

    Crafting Grid remains open and is functional

--- src/main/java/com/teamrune/symbology/items/ItemMatrix.java
+++ src/main/java/com/teamrune/symbology/items/ItemMatrix.java
@@ -1,11 +1,20 @@
 package com.teamrune.symbology.items;
 
 import com.teamrune.symbology.Symbology;
+import com.teamrune.symbology.gui.container.ContainerMatrixCrafter;
+
 import net.minecraft.block.BlockWorkbench;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.IInteractionObject;
 import net.minecraft.world.World;
 
 public class ItemMatrix extends Item
@@ -21,8 +30,50 @@ public class ItemMatrix extends Item
 
     public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
     {
-
-        playerIn.displayGui(new BlockWorkbench.InterfaceCraftingTable(worldIn, playerIn.getPosition()));
+        playerIn.displayGui(new ItemMatrix.InterfaceCraftingTable(worldIn, playerIn.getPosition()));
         return itemStackIn;
+    }
+    
+    public static class InterfaceCraftingTable implements IInteractionObject
+    {
+        private final World world;
+        private final BlockPos position;
+
+        public InterfaceCraftingTable(World worldIn, BlockPos pos)
+        {
+            this.world = worldIn;
+            this.position = pos;
+        }
+
+        /**
+         * Gets the name of this command sender (usually username, but possibly "Rcon")
+         */
+        public String getName()
+        {
+            return null;
+        }
+
+        /**
+         * Returns true if this thing is named
+         */
+        public boolean hasCustomName()
+        {
+            return false;
+        }
+
+        public IChatComponent getDisplayName()
+        {
+            return new ChatComponentTranslation(Blocks.crafting_table.getUnlocalizedName() + ".name", new Object[0]);
+        }
+
+        public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn)
+        {
+            return new ContainerMatrixCrafter(playerInventory, this.world, this.position);
+        }
+
+        public String getGuiID()
+        {
+            return "minecraft:crafting_table";
+        }
     }
 }
