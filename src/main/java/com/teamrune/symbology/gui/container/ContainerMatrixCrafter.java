c2cebac717d49780d0c5c638c1613c3d5edf9739
saxon564 <saxon564@gmail.com>
2015-04-07 00:35:17 -0400

    Cleaned up code a bit.

--- src/main/java/com/teamrune/symbology/gui/container/ContainerMatrixCrafter.java
+++ src/main/java/com/teamrune/symbology/gui/container/ContainerMatrixCrafter.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCraftResult;
 import net.minecraft.inventory.InventoryCrafting;
@@ -14,7 +15,7 @@ import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
-public class ContainerMatrixCrafter extends Container
+public class ContainerMatrixCrafter extends ContainerWorkbench
 {
     /** The crafting matrix inventory (3x3). */
     public InventoryCrafting craftMatrix = new InventoryCrafting(this, 3, 3);
@@ -23,139 +24,14 @@ public class ContainerMatrixCrafter extends Container
     private BlockPos field_178145_h;
     private static final String __OBFID = "CL_00001744";
 
-    public ContainerMatrixCrafter(InventoryPlayer playerInventory, World worldIn, BlockPos p_i45800_3_)
+    public ContainerMatrixCrafter(InventoryPlayer playerInventory, World worldIn, BlockPos blockPos)
     {
-        this.worldObj = worldIn;
-        this.field_178145_h = p_i45800_3_;
-        this.addSlotToContainer(new SlotCrafting(playerInventory.player, this.craftMatrix, this.craftResult, 0, 124, 35));
-        int i;
-        int j;
-
-        for (i = 0; i < 3; ++i)
-        {
-            for (j = 0; j < 3; ++j)
-            {
-                this.addSlotToContainer(new Slot(this.craftMatrix, j + i * 3, 30 + j * 18, 17 + i * 18));
-            }
-        }
-
-        for (i = 0; i < 3; ++i)
-        {
-            for (j = 0; j < 9; ++j)
-            {
-                this.addSlotToContainer(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-            }
-        }
-
-        for (i = 0; i < 9; ++i)
-        {
-            this.addSlotToContainer(new Slot(playerInventory, i, 8 + i * 18, 142));
-        }
-
-        this.onCraftMatrixChanged(this.craftMatrix);
+    	super(playerInventory, worldIn, blockPos);
     }
 
-    /**
-     * Callback for when the crafting matrix is changed.
-     */
-    public void onCraftMatrixChanged(IInventory inventoryIn)
-    {
-        this.craftResult.setInventorySlotContents(0, CraftingManager.getInstance().findMatchingRecipe(this.craftMatrix, this.worldObj));
-    }
-
-    /**
-     * Called when the container is closed.
-     */
-    public void onContainerClosed(EntityPlayer playerIn)
-    {
-        super.onContainerClosed(playerIn);
-
-        if (!this.worldObj.isRemote)
-        {
-            for (int i = 0; i < 9; ++i)
-            {
-                ItemStack itemstack = this.craftMatrix.getStackInSlotOnClosing(i);
-
-                if (itemstack != null)
-                {
-                    playerIn.dropPlayerItemWithRandomChoice(itemstack, false);
-                }
-            }
-        }
-    }
-
+    @Override
     public boolean canInteractWith(EntityPlayer playerIn)
     {
         return true;
-    }
-
-    /**
-     * Take a stack from the specified inventory slot.
-     */
-    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
-    {
-        ItemStack itemstack = null;
-        Slot slot = (Slot)this.inventorySlots.get(index);
-
-        if (slot != null && slot.getHasStack())
-        {
-            ItemStack itemstack1 = slot.getStack();
-            itemstack = itemstack1.copy();
-
-            if (index == 0)
-            {
-                if (!this.mergeItemStack(itemstack1, 10, 46, true))
-                {
-                    return null;
-                }
-
-                slot.onSlotChange(itemstack1, itemstack);
-            }
-            else if (index >= 10 && index < 37)
-            {
-                if (!this.mergeItemStack(itemstack1, 37, 46, false))
-                {
-                    return null;
-                }
-            }
-            else if (index >= 37 && index < 46)
-            {
-                if (!this.mergeItemStack(itemstack1, 10, 37, false))
-                {
-                    return null;
-                }
-            }
-            else if (!this.mergeItemStack(itemstack1, 10, 46, false))
-            {
-                return null;
-            }
-
-            if (itemstack1.stackSize == 0)
-            {
-                slot.putStack((ItemStack)null);
-            }
-            else
-            {
-                slot.onSlotChanged();
-            }
-
-            if (itemstack1.stackSize == itemstack.stackSize)
-            {
-                return null;
-            }
-
-            slot.onPickupFromSlot(playerIn, itemstack1);
-        }
-
-        return itemstack;
-    }
-
-    /**
-     * Called to determine if the current slot is valid for the stack merging (double-click) code. The stack passed in
-     * is null for the initial slot that was double-clicked.
-     */
-    public boolean canMergeSlot(ItemStack p_94530_1_, Slot p_94530_2_)
-    {
-        return p_94530_2_.inventory != this.craftResult && super.canMergeSlot(p_94530_1_, p_94530_2_);
     }
 }
--- src/main/java/com/teamrune/symbology/items/ItemMatrix.java
+++ src/main/java/com/teamrune/symbology/items/ItemMatrix.java
@@ -1,14 +1,10 @@
 package com.teamrune.symbology.items;
 
-import com.teamrune.symbology.Symbology;
-import com.teamrune.symbology.gui.container.ContainerMatrixCrafter;
-
 import net.minecraft.block.BlockWorkbench;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
@@ -16,6 +12,9 @@ import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.IInteractionObject;
 import net.minecraft.world.World;
+
+import com.teamrune.symbology.Symbology;
+import com.teamrune.symbology.gui.container.ContainerMatrixCrafter;
 
 public class ItemMatrix extends Item
 {
@@ -34,46 +33,22 @@ public class ItemMatrix extends Item
         return itemStackIn;
     }
     
-    public static class InterfaceCraftingTable implements IInteractionObject
+    public static class InterfaceCraftingTable extends BlockWorkbench.InterfaceCraftingTable implements IInteractionObject
     {
         private final World world;
         private final BlockPos position;
 
         public InterfaceCraftingTable(World worldIn, BlockPos pos)
         {
+        	super(worldIn, pos);
             this.world = worldIn;
             this.position = pos;
         }
 
-        /**
-         * Gets the name of this command sender (usually username, but possibly "Rcon")
-         */
-        public String getName()
-        {
-            return null;
-        }
-
-        /**
-         * Returns true if this thing is named
-         */
-        public boolean hasCustomName()
-        {
-            return false;
-        }
-
-        public IChatComponent getDisplayName()
-        {
-            return new ChatComponentTranslation(Blocks.crafting_table.getUnlocalizedName() + ".name", new Object[0]);
-        }
-
+        @Override
         public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn)
         {
             return new ContainerMatrixCrafter(playerInventory, this.world, this.position);
-        }
-
-        public String getGuiID()
-        {
-            return "minecraft:crafting_table";
         }
     }
 }
