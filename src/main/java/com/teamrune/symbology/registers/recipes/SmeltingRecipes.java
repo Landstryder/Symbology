744da10dbce03124e92b800c9d31a8e4455c490b
Landstryder <brian@bicyclefrontier.com>
2015-12-14 21:36:37 -0500

    Added smelt recipe for Ash log

--- src/main/java/com/teamrune/symbology/registers/recipes/SmeltingRecipes.java
+++ src/main/java/com/teamrune/symbology/registers/recipes/SmeltingRecipes.java
@@ -2,6 +2,7 @@ package com.teamrune.symbology.registers.recipes;
 
 import com.teamrune.symbology.Symbology;
 import com.teamrune.symbology.items.ItemScribe;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -10,5 +11,6 @@ public class SmeltingRecipes
     public static void init()
     {
         GameRegistry.addSmelting(new ItemStack(Symbology.silver_ore), new ItemStack(Symbology.silver_ingot), (float) .1);
+        GameRegistry.addSmelting(new ItemStack(Symbology.ash_log), new ItemStack(Items.coal, 0, 1), (float) .1);
     }
 }
